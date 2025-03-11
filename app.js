68d9019fb77bab19fa360f4575794aaf24c6f735
tory99 <hjw990826@gmail.com>
2025-03-11 15:31:23 +0900

    03.11 app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+// const db = require('./db');
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const userRouter = require('./routes/users')
+const bookRouter = require('./routes/books')
+const likeRouter = require('./routes/likes')
+const cartRouter = require('./routes/carts')
+const orderRouter = require('./routes/orders')
+
+app.use("/users", userRouter);
+app.use("/books", bookRouter);
+app.use("/likes", likeRouter);
+app.use("/cart", cartRouter);
+app.use("/order", orderRouter);
+
+
+
+app.listen(process.env.PORT);
