4e3cdcfdf23861b1743c87eef7bc310c2ff3d5cf
tory99 <hjw990826@gmail.com>
2025-03-15 18:01:52 +0900

    03.15

--- controller/Bookcontroller.js
+++ controller/Bookcontroller.js
@@ -2,42 +2,44 @@ const db = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 
 const allBooks = (req,res) => {
-    let {category_id} = req.query;
-    
-    if(category_id){
-        let sql = "SELECT * FROM books WHERE category_id = ?";
-        db.query(sql, [category_id], (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
+    let { category_id, news, limit, currentPage } = req.query;
 
-            if(result.length){
-                return res.status(StatusCodes.OK).json(results);
-            } else{
-                return res.status(StatusCodes.NOT_FOUND).end()
-            }
-       }
-    )
-    } else {
-        let sql = "SELECT * FROM books";
-        db.query(sql, (err, results) => {
-            if(err) {
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
-            }
-    
-                return res.status(StatusCodes.CREATED).json(results);
-            }
-        )}
+    let offset = limit * (currentPage - 1 );
+    let values = []
+    let sql = "SELECT * FROM books"
 
-    
+
+    if (category_id && news){
+        sql += " WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+        values = [category_id];
+    } else if(category_id){
+        sql += " WHERE category_id = ?";
+        values = [category_id];
+    } else if(news){
+        sql += " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    }
+
+    sql += " LIMIT ? OFFSET ?"
+    values.push(parseInt(limit), offset);
+
+    db.query(sql, values, (err, results) => {
+        if(err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end();
+        }
+
+        if(results.length){
+            return res.status(StatusCodes.OK).json(results);
+        } else{
+            return res.status(StatusCodes.NOT_FOUND).end()
+        }
+    })
 };
 
 const bookDetail = (req,res) => {
     let {id} = req.params;
 
-    let sql = "SELECT * FROM books WHERE id = ?";
+    let sql = "SELECT * FROM books LEFT JOIN category ON books.category_id = category.id WHERE books.id=?;";
     db.query(sql, [id], (err, results) => {
             if(err) {
                 console.log(err);
